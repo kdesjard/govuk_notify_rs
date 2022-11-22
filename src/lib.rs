fb1283b485395ac9df8eb05855c556fcd2e392bc
baarkerlounger <db@slothlife.xyz>
2022-11-22 18:20:44 +0000

    Add doc.rs comments

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,22 @@
+//! # GOV.UK Notify Rust client
+//!
+//! Use this (unofficial) client to send emails using the
+//! [GOV.UK Notify](https://www.notifications.service.gov.uk) API. See the linked documentation to
+//! obtain an API key.
+//
+//! ```rust
+//! use govuk_notify::NotifyClient;
+//! use serde_json::{Map, Value};
+//!
+//! let notify_client = NotifyClient::new(api_key);
+//! let mut personalisation = Map::new();
+//! let mut personalisation_values = Map::new();
+//! personalisation_values.insert("my_var".to_string(), Value::String("my value".to_string()));
+//! personalisation.insert("personalisation".to_string(), Value::Object(personalisation_values));
+//!
+//! notify_client.send_email(email, template_id, Some(personalisation)).await
+//! ```
+
 use chrono::Utc;
 use jsonwebtoken::{encode, Algorithm, EncodingKey, Header};
 use reqwest;
